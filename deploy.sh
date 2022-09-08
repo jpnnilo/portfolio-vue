1955f4f6e626f72befda40ae74c57be4e8a6a787
jpnnilo <53546940+jpnnilo@users.noreply.github.com>
2022-09-09 01:06:49 +0800

    update ignore

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push git@github.com:jpnnilo/portfolio-vue.git master:gh-pages
+git push git@github.com:jpnnilo/portfolio-vue.git main:gh-pages
 
 cd -
